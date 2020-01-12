9b1103d9b370eab488564c89a46ad7e6e1ac47b2
Alex Deviban <alex@gmail.com>
2020-01-12 13:53:28 +0330

    First commit

---
+++ precache-manifest.fdfddc0d091ff944ed16a0b198906b6d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ecd5be83902eb2d69aae001f7e7371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bacdd067eb8a0589c06f",
+    "url": "/static/css/main.c3a83263.chunk.css"
+  },
+  {
+    "revision": "48ef90fb4d4cd7b90f10",
+    "url": "/static/js/2.4b334c42.chunk.js"
+  },
+  {
+    "revision": "da01034c70d479d890a70d42d48c6305",
+    "url": "/static/js/2.4b334c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bacdd067eb8a0589c06f",
+    "url": "/static/js/main.07e29cec.chunk.js"
+  },
+  {
+    "revision": "9f026fabe2dcb4909710",
+    "url": "/static/js/runtime-main.f6ac920b.js"
+  },
+  {
+    "revision": "d5c4860bebf2101616d19b3e205afa6e",
+    "url": "/static/media/AppleTv.d5c4860b.png"
+  },
+  {
+    "revision": "82c85d8ec546ca0624aca49853884a2d",
+    "url": "/static/media/Aurmatic.82c85d8e.png"
+  },
+  {
+    "revision": "4a88b3338281a0a889b8bf94f2e8544d",
+    "url": "/static/media/DarkSky.4a88b333.png"
+  },
+  {
+    "revision": "bca431fe10ad9d76ea5b4b36ea9751a3",
+    "url": "/static/media/Facetune.bca431fe.png"
+  },
+  {
+    "revision": "7c192fa93fbecbfb6a97736406a39112",
+    "url": "/static/media/FitOn.7c192fa9.png"
+  },
+  {
+    "revision": "fe7ba4fd4f8ab5702404ff078041e191",
+    "url": "/static/media/Scrivener.fe7ba4fd.png"
+  },
+  {
+    "revision": "8e7d959cf662dd4cf2bb260423c69b0a",
+    "url": "/static/media/Sibapp.8e7d959c.jpg"
+  },
+  {
+    "revision": "70a2cc52f0982b2eb2f6c3f4e4ba58b3",
+    "url": "/static/media/Xmart.70a2cc52.png"
+  },
+  {
+    "revision": "54ab74d61f0c85207e7178044071a6b0",
+    "url": "/static/media/iContacts.54ab74d6.png"
+  }
+]);
